5974ea689ba0fe36c935d1e2deed1dd6b379770f
polagmnoz <paulaagarcia1@gmail.com>
2017-04-24 11:13:20 -0500

    Creación de clases

---
+++ Branches/src/BranchesMain.java
@@ -0,0 +1,30 @@
+import processing.core.PApplet;
+
+public class BranchesMain extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("BranchesMain");
+
+	}
+	
+	@Override
+	public void settings() {
+		fullScreen();
+	}
+	
+	@Override
+	public void setup() {
+		
+	}
+	
+	@Override
+	public void draw() {
+		
+	}
+	
+	@Override
+	public void keyPressed() {
+		
+	}
+	
+}
