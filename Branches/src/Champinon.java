94695d5a6ec0dfc2dee97bf1607a089d392bc310
polagmnoz <paulaagarcia1@gmail.com>
2017-04-24 12:20:32 -0500

    Metodos champiñones

--- Branches/src/Champinon.java
+++ Branches/src/Champinon.java
@@ -1,4 +1,46 @@
+import processing.core.PApplet;
 
-public class Champinon {
+public class Champinon implements Runnable {
+	
+	private float x,y;
+	public boolean empezar;
+	public Champinon() {
+	}
+	
+
+	/*
+	 * Método que nos pintara los champiñones
+	 */
+	public void pintar(PApplet app) {
+
+	}
+	
+	/*
+	 * Método que nos pintara los champiñones 
+	 * en la clase Pote cuando son recogidos por el usuario
+	 * 
+	 */
+	public void pintar(PApplet app, float nuevaX, float nuevoY) {
+
+	}
+	
+	/*
+	 * Método que contendrá el hilo de la caida de los champiñones
+	 * */
+	
+	public void run() {
+		
+		
+	}
+	
+	/*
+	 * Método que se encargará de animar la caida de los champiñones 
+	 * una vez el usuario empiece a interactuar en la pantalla
+	 * */
+	public void caida(){
+		
+	}
+	
+	
 
 }
